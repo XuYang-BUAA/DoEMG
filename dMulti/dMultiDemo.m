208171bdc33745542bcf27b1a214b795b52e18b8
XuYang-BUAA <xu.yang@sjtu.edu.cn>
2020-07-09 20:08:19 +0800

    test

--- dMulti/dMultiDemo.m
+++ dMulti/dMultiDemo.m
@@ -29,10 +29,41 @@ sEMG.chn_num = [40,20];
 [~,N] = size(sEMG.data{1,1});
 t = (sEMG.t0:N-1)' * sEMG.dt;
 
-select_ch = [20,10];
+for j=1:20
+    for i=1:39
+        sEMG.data{i,j}=sEMG.data{i,j}-sEMG.data{i+1,j};
+    end
+end
 figure
-plot(t,sEMG.data{select_ch(1),select_ch(2)})
-hold on
+for j=1
+    for i=1:20
+        select_ch = [j,i];
+        plot(t,sEMG.data{select_ch(1),select_ch(2)}+100*i+300*j)
+        hold on
+    end
+end
+% for i=1:39
+%     sEMG.data{i,9}=sEMG.data{i,9}-sEMG.data{i+1,9};
+% end
+
+% sEMG.data{20,8}=sEMG.data{20,8}-sEMG.data{20,9};
+% sEMG.data{20,10}=sEMG.data{20,10}-sEMG.data{20,9};
+% sEMG.data{20,9}=sEMG.data{20,9}-sEMG.data{19,9};
+
+
+t = (sEMG.t0:N-1)' * sEMG.dt;
+
+figure
+for i=1:39
+    select_ch = [i,9];
+    plot(t,sEMG.data{select_ch(1),select_ch(2)}+100*i+100*40)
+    hold on
+end
+% for i=1:39
+%     select_ch = [i,10];
+%     plot(t,sEMG.data{select_ch(1),select_ch(2)}+100*(i+41))
+%     hold on
+% end
 
 
 
